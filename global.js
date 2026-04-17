f9144e987562d201ce1af7f48ffdd85d107dd3ad
Yall777 <75231012+Yall777@users.noreply.github.com>
2026-04-17 13:59:37 -0700

    edit

--- global.js
+++ global.js
@@ -4,10 +4,30 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"
+  : "/Portfolio-DSC-106/";
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'https://github.com/Yall777/Portfolio-DSC-106', title: 'GitHub' },
+];
 
-currentLink?.classList.add('current');
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  a.classList.toggle('current', a.host === location.host && a.pathname === location.pathname);
+  a.toggleAttribute('target', a.host !== location.host);
+  nav.append(a);
+}
